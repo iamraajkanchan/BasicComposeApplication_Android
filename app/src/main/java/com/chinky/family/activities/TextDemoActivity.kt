218c1f39bf7863e906c053c894a747ddd32cd187
iamraajkanchan <iamraajkanchan@gmail.com>
2025-06-08 17:48:39 +0530

    Changes made to add examples of Text composable.

--- app/src/main/java/com/chinky/family/activities/TextDemoActivity.kt
+++ app/src/main/java/com/chinky/family/activities/TextDemoActivity.kt
@@ -1,10 +1,12 @@
 package com.chinky.family.activities
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,8 +17,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.chinky.family.ui.theme.ApplicationColor
+import com.chinky.family.ui.theme.ApplicationFont
 import com.chinky.family.ui.theme.ApplicationTheme
 
 class TextDemoActivity : ComponentActivity() {
@@ -34,6 +40,13 @@ class TextDemoActivity : ComponentActivity() {
 
     @Composable
     private fun displayVariousTextOptions(padding: PaddingValues) {
+        // Note: You can only find LocalContext.current in a composable and this is the only way to declare and define a context.
+        // In a ComponentActivity, you won't find context in onCreate, onStart, onResume, onPause, onStop and onDestroy overridden methods.
+        //
+        val context = LocalContext.current
+        // In a ComponentActivity, you won't find applicationContext in onCreate, onStart, onResume, onPause, onStop and onDestroy overridden methods.
+        val applicationContext = LocalContext.current.applicationContext
+
         Column(
             modifier = Modifier
                 .padding(padding)
@@ -41,13 +54,35 @@ class TextDemoActivity : ComponentActivity() {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             // You don't need a comma to add another composable
-            Text("Just a Normal Text with no modifier")
+            Text("Normal Text with no modifier")
             Text(
-                "Just a Normal Text with background and padding",
-                modifier = Modifier.background(ApplicationColor.PurpleGrey80).padding(8.dp)
+                "Normal Text with background and padding",
+                modifier = Modifier
+                    .background(ApplicationColor.PurpleGrey80)
+                    .padding(8.dp)
 
             )
-            Text("Just a Normal Text with no modifier")
+            Text(
+                "Normal Text with modified color and font size",
+                color = ApplicationColor.Orange,
+                fontSize = 16.sp
+            )
+            Text(
+                "Normal Text with bold font and weight",
+                color = ApplicationColor.Blue,
+                fontSize = 16.sp,
+                fontFamily = ApplicationFont.robotoBold,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                "Clickable Text with italic font",
+                color = ApplicationColor.Green,
+                fontSize = 16.sp,
+                fontFamily = ApplicationFont.robotoItalic,
+                modifier = Modifier.clickable {
+                    Toast.makeText(context, "Clicked Green Text", Toast.LENGTH_SHORT).show()
+                }
+            )
         }
     }
 }
--- app/src/main/java/com/chinky/family/ui/theme/Color.kt
+++ app/src/main/java/com/chinky/family/ui/theme/Color.kt
@@ -10,4 +10,8 @@ object ApplicationColor {
     val Purple40 = Color(0xFF6650a4)
     val PurpleGrey40 = Color(0xFF625b71)
     val Pink40 = Color(0xFF7D5260)
+    val Orange = Color(0xFFFF9800)
+    val White = Color(0xFFFFFFFF)
+    val Blue = Color(0xFF0AA2FA)
+    val Green = Color(0xFF2AFA0A)
 }
