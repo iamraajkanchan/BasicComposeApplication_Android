f93682c525378f8566d1ff3942250460353954ce
iamraajkanchan <iamraajkanchan@gmail.com>
2025-06-10 21:03:02 +0530

    Working on examples of various columns in ColumnDemoActivity.kt

--- app/src/main/java/com/chinky/family/activities/ColumnDemoActivity.kt
+++ app/src/main/java/com/chinky/family/activities/ColumnDemoActivity.kt
@@ -4,10 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.chinky.family.ui.theme.ApplicationColor
 import com.chinky.family.ui.theme.ApplicationTheme
 
 class ColumnDemoActivity : ComponentActivity() {
@@ -17,14 +27,37 @@ class ColumnDemoActivity : ComponentActivity() {
         setContent {
             ApplicationTheme.ChildApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { padding ->
-                    displayVariousTextOptions()
+                    DisplayVariousColumnOptions(padding)
                 }
             }
         }
     }
 
     @Composable
-    private fun displayVariousTextOptions() {
+    private fun DisplayVariousColumnOptions(paddingValues: PaddingValues) {
+        CreateColumnWithFixedHeightAndMaximumWidth(paddingValues)
+        CreateColumnWithFixedWidthAndMaximumHeight(paddingValues)
+    }
 
+    @Composable
+    private fun CreateColumnWithFixedHeightAndMaximumWidth(paddingValues: PaddingValues) {
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxWidth()
+                .height(100.dp)
+                .background(color = ApplicationColor.Pink40)
+        ) { }
+    }
+
+    @Composable
+    private fun CreateColumnWithFixedWidthAndMaximumHeight(paddingValues: PaddingValues) {
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxHeight()
+                .width(100.dp)
+                .background(color = ApplicationColor.Purple40)
+        ) { }
     }
 }
