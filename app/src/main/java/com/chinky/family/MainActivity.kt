51b9ca72462d110a4cda74dc83e79c6b8559850b
Raj Kanchan <raj.kanchan@quantsapp.com>
2025-06-10 17:57:41 +0530

    Changes made to add all the composable widgets in Main Menu.

--- app/src/main/java/com/chinky/family/MainActivity.kt
+++ app/src/main/java/com/chinky/family/MainActivity.kt
@@ -11,10 +11,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,6 +27,23 @@ import com.chinky.family.activities.ButtonDemoActivity
 import com.chinky.family.activities.CardDemoActivity
 import com.chinky.family.activities.CheckboxDemoActivity
 import com.chinky.family.activities.CircularProgressIndicatorDemoActivity
+import com.chinky.family.activities.ColumnDemoActivity
+import com.chinky.family.activities.IconButtonDemoActivity
+import com.chinky.family.activities.IconDemoActivity
+import com.chinky.family.activities.ImageDemoActivity
+import com.chinky.family.activities.LazyColumnDemoActivity
+import com.chinky.family.activities.LazyRowDemoActivity
+import com.chinky.family.activities.LinearProgressIndicatorDemoActivity
+import com.chinky.family.activities.ListItemDemoActivity
+import com.chinky.family.activities.ModifierDemoActivity
+import com.chinky.family.activities.OutlinedButtonDemoActivity
+import com.chinky.family.activities.OutlinedTextFieldDemoActivity
+import com.chinky.family.activities.RadioButtonDemoActivity
+import com.chinky.family.activities.RowDemoActivity
+import com.chinky.family.activities.ScaffoldDemoActivity
+import com.chinky.family.activities.SnackbarDemoActivity
+import com.chinky.family.activities.SwitchDemoActivity
+import com.chinky.family.activities.TextButtonDemoActivity
 import com.chinky.family.activities.TextDemoActivity
 import com.chinky.family.activities.TextFieldDemoActivity
 import com.chinky.family.ui.theme.ApplicationTheme
@@ -150,6 +169,185 @@ class MainActivity : ComponentActivity() {
                     }
                 ) {
                     Text("Circular Progress")
+                }
+            }
+            Row {
+                Button(
+                    modifier = leftButtonModifier,
+                    onClick = {
+                        val textActivityIntent =
+                            Intent(this@MainActivity, ColumnDemoActivity::class.java)
+                        startActivity(textActivityIntent)
+                    }
+                ) {
+                    Text("Column")
+                }
+
+                Button(
+                    modifier = rightButtonModifier,
+                    onClick = {
+                        val textActivityIntent = Intent(
+                            this@MainActivity,
+                            IconButtonDemoActivity::class.java
+                        )
+                        startActivity(textActivityIntent)
+                    }
+                ) {
+                    Text("Icon Button")
+                }
+            }
+            Row {
+                Button(
+                    modifier = leftButtonModifier,
+                    onClick = {
+                        val textActivityIntent =
+                            Intent(this@MainActivity, IconDemoActivity::class.java)
+                        startActivity(textActivityIntent)
+                    }
+                ) {
+                    Text("Icon")
+                }
+
+                Button(
+                    modifier = rightButtonModifier,
+                    onClick = {
+                        val textActivityIntent =
+                            Intent(this@MainActivity, ImageDemoActivity::class.java)
+                        startActivity(textActivityIntent)
+                    }
+                ) {
+                    Text("Image")
+                }
+            }
+            Row {
+                Button(
+                    modifier = leftButtonModifier,
+                    onClick = {
+                        val textActivityIntent =
+                            Intent(this@MainActivity, LazyColumnDemoActivity::class.java)
+                        startActivity(textActivityIntent)
+                    }
+                ) {
+                    Text("Lazy Column")
+                }
+
+                Button(
+                    modifier = rightButtonModifier,
+                    onClick = {
+                        val textActivityIntent = Intent(
+                            this@MainActivity, LazyRowDemoActivity::class.java
+                        )
+                        startActivity(textActivityIntent)
+                    }
+                ) {
+                    Text("Lazy Row")
+                }
+            }
+            Row {
+                Button(
+                    modifier = leftButtonModifier,
+                    onClick = {
+                        val textActivityIntent =
+                            Intent(this@MainActivity, ModifierDemoActivity::class.java)
+                        startActivity(textActivityIntent)
+                    }
+                ) {
+                    Text("Modifier")
+                }
+
+                Button(
+                    modifier = rightButtonModifier,
+                    onClick = {
+                        val textActivityIntent =
+                            Intent(this@MainActivity, OutlinedButtonDemoActivity::class.java)
+                        startActivity(textActivityIntent)
+                    }
+                ) {
+                    Text("Outlined Button")
+                }
+            }
+            Row {
+                Button(
+                    modifier = leftButtonModifier,
+                    onClick = {
+                        val textActivityIntent =
+                            Intent(this@MainActivity, OutlinedTextFieldDemoActivity::class.java)
+                        startActivity(textActivityIntent)
+                    }
+                ) {
+                    Text("Outlined Text Field ")
+                }
+
+                Button(
+                    modifier = rightButtonModifier,
+                    onClick = {
+                        val textActivityIntent =
+                            Intent(this@MainActivity, RadioButtonDemoActivity::class.java)
+                        startActivity(textActivityIntent)
+                    }
+                ) {
+                    Text("Radio Button")
+                }
+
+            }
+            Row {
+                Button(
+                    modifier = leftButtonModifier,
+                    onClick = {
+                        val textActivityIntent =
+                            Intent(this@MainActivity, RowDemoActivity::class.java)
+                        startActivity(textActivityIntent)
+                    }
+                ) {
+                    Text("Row")
+                }
+
+                Button(
+                    modifier = rightButtonModifier,
+                    onClick = {
+                        val textActivityIntent =
+                            Intent(this@MainActivity, ScaffoldDemoActivity::class.java)
+                        startActivity(textActivityIntent)
+                    }
+                ) {
+                    Text("Scaffold")
+                }
+
+            }
+            Row {
+                Button(
+                    modifier = leftButtonModifier,
+                    onClick = {
+                        val textActivityIntent =
+                            Intent(this@MainActivity, SnackbarDemoActivity::class.java)
+                        startActivity(textActivityIntent)
+                    }
+                ) {
+                    Text("Snackbar")
+                }
+
+                Button(
+                    modifier = rightButtonModifier,
+                    onClick = {
+                        val textActivityIntent =
+                            Intent(this@MainActivity, SwitchDemoActivity::class.java)
+                        startActivity(textActivityIntent)
+                    }
+                ) {
+                    Text("Switch")
+                }
+
+            }
+            Row {
+                Button(
+                    modifier = leftButtonModifier,
+                    onClick = {
+                        val textActivityIntent =
+                            Intent(this@MainActivity, TextButtonDemoActivity::class.java)
+                        startActivity(textActivityIntent)
+                    }
+                ) {
+                    Text("Text Button")
                 }
             }
         }
