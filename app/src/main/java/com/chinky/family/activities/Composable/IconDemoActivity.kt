c6d3721acb7c3ae95cf331bf804b88bf7ae9a65f
Raj Kanchan <raj.kanchan@quantsapp.com>
2025-06-12 10:35:16 +0530

    Changes made to add various icon examples in IconDemoActivity.kt

--- app/src/main/java/com/chinky/family/activities/Composable/IconDemoActivity.kt
+++ app/src/main/java/com/chinky/family/activities/Composable/IconDemoActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
@@ -15,13 +16,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import com.chinky.family.R
@@ -56,6 +60,10 @@ class IconDemoActivity : ComponentActivity() {
             ModifiedIcon()
             Spacer(Modifier.height(spacing))
             IconWithClick(context)
+            Spacer(Modifier.height(spacing))
+            BasicDrawableIcon(context)
+            Spacer(Modifier.height(spacing))
+            ModifiedDrawableIcon(context)
         }
     }
 
@@ -83,6 +91,37 @@ class IconDemoActivity : ComponentActivity() {
             contentDescription = "User Profile",
             modifier = Modifier
                 .clickable {
+                    Toast.makeText(context, "You clicked icon with click!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+                .size(50.dp)
+        )
+    }
+
+    @Composable
+    private fun BasicDrawableIcon(context: Context) {
+        Icon(
+            painter = painterResource(R.drawable.ic_user_svg),
+            contentDescription = "User Profile",
+            modifier = Modifier
+                .clickable {
+                    Toast.makeText(context, "You clicked icon with click!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+                .size(50.dp)
+        )
+    }
+
+    @Composable
+    private fun ModifiedDrawableIcon(context: Context) {
+        Icon(
+            painter = painterResource(R.drawable.ic_user_svg),
+            contentDescription = "User Profile",
+            modifier = Modifier
+                .clickable(
+                    indication = rememberRipple(color = ApplicationColor.Orange), // Change ripple color here
+                    interactionSource = remember { MutableInteractionSource() }
+                ) {
                     Toast.makeText(context, "You clicked icon with click!", Toast.LENGTH_SHORT)
                         .show()
                 }
