7b731af9ec5fae14b0a8ab7bb8505ef7e3ff45cd
iamraajkanchan <iamraajkanchan@gmail.com>
2025-08-16 20:17:23 +0530

    Adding Tutorials pages from Jayant's YouTube Tutorial videos.

--- app/src/main/java/com/chinky/family/presentation/ui/jayantTutorial/JTutorialPart4Activity.kt
+++ app/src/main/java/com/chinky/family/presentation/ui/jayantTutorial/JTutorialPart4Activity.kt
@@ -12,20 +12,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalContext
 import com.chinky.family.presentation.ui.common.AppTopBar
+import com.chinky.family.presentation.ui.theme.ApplicationColor
 import com.chinky.family.presentation.ui.theme.ApplicationTheme
-import dagger.hilt.android.internal.Contexts
 
 /**
  * In this tutorial we are going to learn, how to define a Top
@@ -46,30 +45,36 @@ class JTutorialPart4Activity : ComponentActivity() {
     }
     @Composable
     fun ShowLoginScreen(paddingValues: PaddingValues) {
-        var username by remember { mutableStateOf("") }
-        var password by remember { mutableStateOf("") }
+        val username = remember { mutableStateOf("") }
+        val password = remember { mutableStateOf("") }
+        val usernameFocusRequester = remember { FocusRequester() }
+        val usernameBorderColor = remember { mutableStateOf(ApplicationColor.Orange) }
         val context = LocalContext.current
         Column(
             modifier = Modifier.padding(paddingValues).fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally) {
             OutlinedTextField(
-                value = username,
-                onValueChange = { username = it },
-                label = { Text("Username") }
+                value = username.value,
+                onValueChange = { username.value = it },
+                label = { Text("Username") },
+                modifier = Modifier.focusRequester(usernameFocusRequester)
             )
             OutlinedTextField(
-                value = password,
-                onValueChange = { password = it },
+                value = password.value,
+                onValueChange = { password.value = it },
                 label = { Text("Password") }
             )
-            OutlinedButton(onClick = {login(username, password, context)}) { Text("Login") }
+            OutlinedButton(onClick = {login(username, password, usernameFocusRequester,context)}) { Text("Login") }
         }
     }
 
-    private fun login(username: String, password: String, context: Context) {
-        if (username.isNotEmpty() && password.isNotEmpty()) {
-            if (username == "chinky" && password.equals("minky")) {
+    private fun login(username: MutableState<String>, password: MutableState<String>, usernameFocusRequester : FocusRequester ,context: Context) {
+        if (username.value.isNotEmpty() && password.value.isNotEmpty()) {
+            if (username.value == "chinky" && password.value.equals("minky")) {
                 Toast.makeText(context, "Login Successful", Toast.LENGTH_SHORT).show()
+                username.value = ""
+                password.value = ""
+                usernameFocusRequester.requestFocus()
             } else {
                 Toast.makeText(context, "Login Failed", Toast.LENGTH_SHORT).show()
             }
